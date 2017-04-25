f23d99e8d3294313dff20937fb475202168acda1
Mohit B V <bvmohituppsala@gmail.com>
2017-04-25 16:08:20 +0200

    Update priority.c

--- priority scheduling program/priority.c
+++ priority scheduling program/priority.c
@@ -2,7 +2,12 @@
 
 int main()
 {
-   int n, i, j, wt[10], bt[10], pr[10], proc[10]= {0}, tat[10], total, temp;
+   int n, i, j, t = 0, wt[10], at[10], ct[10], bt[10], pr[10], proc[10]= {0}, tat[10], total, temp;
+    //bt is the burst time
+   //ct is the completion time
+   //at is the arrival time
+   //wt is the waiting time
+   //tat is the turn around time
    printf("enter the number of process\n");
    scanf("%d", &n);
    printf("enter the burst time, priority for each task\n");
@@ -10,6 +15,14 @@ int main()
    {
        scanf("%d", &bt[i]);
        scanf("%d", &pr[i]);
+       proc[i] = i+1;//stores the process index
+       
+   }
+     printf("enter the arrival time for each task\n");
+   for(i = 0; i <n ;i++)
+   {  
+       printf("proc[%d]:",i+1);
+       scanf("%d", &at[i]);
    }
    
    printf("\nprocess\tburst time\tpriority\n");
@@ -17,13 +30,14 @@ int main()
    {
        printf("proc[%d]\t \t%d\t \t%d\n", i+1, bt[i], pr[i]);
    }
-   printf("\n");   
-   for(i = 0; i < n ;i++)
+   printf("\n");  
+   //selection sort is used to select the process with less priority   
+   for(i = 1; i < n ;i++)
    {
        int iMax = i;
        for(j =i+1; j<n ;j++)
        {
-           if(pr[j] < pr[iMax])
+           if(pr[j] > pr[iMax])
            iMax = j;
        }
        
@@ -42,38 +56,46 @@ int main()
        
    }
 
-     printf("\nafter\n");
+     
+  //displays the value after the sorting
      printf("\nprocess\tburst time\tpriority\n");
    for(i =0; i< n;i++)
    {
-       printf("proc[%d]\t \t%d\t \t%d\n", i+1, bt[i], pr[i]);
+       printf("proc%d\t \t%d\t \t%d\n", proc[i], bt[i], pr[i]);
    }
    
    wt[0] = 0;
    
-   for(i = 1; i< n ;i++)
-   {
-       wt[i] = 0;
-       for(j = 0; j< i; j++)
-       {
-           wt[i] = wt[i] + bt[j];
-           
-       }
-       
-   }
-   
-   
-   total = 0;
    for(i =0; i<n ; i++)
    {
-       tat[i] =  wt[i] + bt[i];
-               
+       if(at[0] >= 1)
+       {
+        t =  t + at[0];//scheduler remains idle without incrementing the time
+        
+       }
+      
+       
+       t = t + bt[i];// adds the time process for each task
+       ct[i] = t;
+       //t = t - at[0];//to balance the scheduler task
+   }
+
+   for(i =0; i<n ; i++)
+   {
+       tat[i] = ct[i] - at[i];//turn around time is difference between completion time and arrival time 
+      
+       
+   }
+    for(i =0; i<n ; i++)
+   {
+       wt[i] = tat[i] - bt[i];//waiting time is the difference between turn around time and burn time
+       
    }
    int avtat = total/n;
-   printf("\nprocess\tburst time\tpriority\twaiting time\tturnaround time\n");
+   printf("\nprocess\tburst time\tcompletion time\twaiting time\tturnaround time\n");
    for(i = 0; i <n;i++)
    {
-       printf("proc[%d]\t\t%d\t\t%d\t\t%d\t\t%d\n", i+1, bt[i], pr[i], wt[i], tat[i] );
+       printf("proc%d\t\t%d\t\t%d\t\t%d\t\t\t%d\n", proc[i], bt[i], ct[i], wt[i], tat[i] );
    }
    
     return 0;
