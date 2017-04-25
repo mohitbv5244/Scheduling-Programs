c40838b42909999a77e27db6957764f3f4c00cc1
Mohit B V <bvmohituppsala@gmail.com>
2017-04-25 14:13:58 +0200

    Update first come first serve.c

--- first come first serve/first come first serve.c
+++ first come first serve/first come first serve.c
@@ -4,11 +4,14 @@
 void main()
 {
 
-int i, j, n, bt[10], wt[10], tat[10], awt = 0, atat = 0;
-
+int i, j, n, bt[10], at[10], rem_bt[10], ct[10], wt[10], tat[10],  t = 0;
+float  awt = 0, atat = 0;
 //n is number of tasks
 //bt is the process time for each task
 //wt is the waiting tie for each task
+//ct is the completion time for each task
+//t is the run-time for each task
+//tat is the turn around time for each task
 //awt is the average waiting time
 //atat is the average turn around time
 
@@ -19,24 +22,39 @@ printf("enter the process time for each task\n");
 for(i = 0; i < n; i++)
 {
 scanf("%d", &bt[i]);
+rem_bt[i] = bt[i];
 }
 
+printf("enter the arrival time for each task\n");
+for(i = 0; i < n; i++)
+{
+scanf("%d", &at[i]);
+}
 wt[0] = 0;// first task should not wait
 
-for(i = 1; i< n; i++)
-{
-    wt[i] = 0;
- for(j =0; j < i; j++)
- {
-  wt[i] = wt[i] + bt[j]; // to calculate waiting time
- }
-}
+
 
 
 for(i = 0; i < n; i++)
 {
-tat[i] = bt[i] + wt[i]; // to calculate turn around time
+ t = t+ bt[i];//computes time t for eac task(run-time)
+ ct[i] = t;//t is the actual value for the completion time
 }
+
+
+
+
+
+
+for(i = 0; i < n; i++)
+{
+tat[i] = ct[i] - at[i]; // to calculate turn around time
+}
+for(i = 0; i < n; i++)
+{
+ wt[i] = tat[i] - bt[i];// to calculate waiting time
+}
+
 
 for(i = 0; i < n ; i++)
 {
@@ -46,16 +64,13 @@ atat += tat[i]; // to calculate average turn around time
 
 awt = awt/n;
 atat = atat/n;
-printf("\n process time\tBurst time\tWaiting time\tTurnaround Time");
-
-for(i = 0; i < n; i++)
+printf("\nprocess time\tBurst time\tarrival time\t completion time\tWaiting time\tTurnaround Time");
+for(i = 0; i< n ;i++)
 {
-// to print the tasks time
-
-printf("\n%d\t\t%d\t\t%d\t\t%d\t\t",i+1, bt[i], wt[i], tat[i]);
+printf("p[%d]\t\t\t%d\t\t\t%d\t\t\t%d\t\t\t%d\t\t\t%d\n", i+1, bt[i], at[i], ct[i], wt[i], tat[i]);
 }
 
-printf("\nthe average waitng time and average turn around time are %d and  %d \t \n", awt, atat);
+printf("\nthe average waitng time and average turn around time are %f and  %f \t \n", awt, atat);
 
 
 }
